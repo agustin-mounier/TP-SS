5fc98e01d131254187d940d2bdc0486dd178e90b
agustin-mounier <agusmouni@gmail.com>
2017-03-12 21:01:18 -0300

    added periodic boundry logic and deterministic calculation for optimus M parameter

--- src/cellindexmethod/CellIndexMethod.java
+++ src/cellindexmethod/CellIndexMethod.java
@@ -3,10 +3,7 @@ package cellindexmethod;
 import models.Particle;
 import models.Point;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by sebastian on 3/12/17.
@@ -22,14 +19,16 @@ public class CellIndexMethod {
     private int m;
 
     private List<Particle> particles;
+    private boolean periodicBoundry = false;
+    private Map<Integer, Set<Integer>> neighbours = new HashMap<>();
 
-    // TODO: What's "condiciones periodicas de contorno"
-    public CellIndexMethod(double l, double rc, int m, List<Particle> particles) {
+    public CellIndexMethod(double l, double rc, int m, List<Particle> particles, boolean periodicBoundry) {
         this.l = l;
         this.rc = rc;
         this.m = m;
-        cellLenght = l/m;
+        cellLenght = l / m;
         this.particles = particles;
+        this.periodicBoundry = periodicBoundry;
         insertParticles(m, particles);
     }
 
@@ -43,7 +42,7 @@ public class CellIndexMethod {
         for (Particle p :
                 particles) {
             Point position = p.getPosition();
-            matrix[(int)(position.x / cellLenght)][(int)(position.y / cellLenght)].add(p);
+            matrix[(int) (position.x / cellLenght)][(int) (position.y / cellLenght)].add(p);
         }
     }
 
@@ -54,43 +53,118 @@ public class CellIndexMethod {
         int cellY = (int) (point.y / cellLenght);
         Set<Particle> neighbours = new HashSet<>();
 
-        for(int i = cellX - 1; i <= cellX + 1; i ++) {
-            if ( i < 0 || i >= m) continue;
+        for (int i = cellX - 1; i <= cellX + 1; i++) {
+            if (!periodicBoundry && (i < 0 || i >= m)) continue;
+
             for (int j = cellY - 1; j <= cellY + 1; j++) {
-                if (j < 0 || j >= m) continue;
-                neighbours.addAll(matrix[i][j]);
+                if (!periodicBoundry && (j < 0 || j >= m)) continue;
+
+                if (periodicBoundry)
+                    addNeighboursWithPeriodicBoundries(neighbours, i, j);
+                else
+                    neighbours.addAll(matrix[i][j]);
             }
         }
         neighbours.remove(particle);
         return neighbours;
     }
 
+    private void addNeighboursWithPeriodicBoundries(Set<Particle> neighbours, int i, int j) {
+        Set<Particle> neighbourCell = new HashSet<>();
+        double deltaX = 0;
+        double deltaY = 0;
+
+        if (i < 0 && j < 0) {
+            neighbourCell = matrix[i + m][j + m];
+            deltaX = l * -1;
+            deltaY = l * -1;
+        } else if (i >= m && j >= m) {
+            neighbourCell = matrix[i - m][j - m];
+            deltaX = l;
+            deltaY = l;
+        } else if (i >= m && j < 0) {
+            neighbourCell = matrix[i - m][j + m];
+            deltaX = l;
+            deltaY = l * -1;
+        } else if (i < 0 && j >= m) {
+            neighbourCell = matrix[i + m][j - m];
+            deltaX = l * -1;
+            deltaY = l;
+        } else if (i < 0 && (j >= 0 && j < m)) {
+            neighbourCell = matrix[i + m][j];
+            deltaX = l * -1;
+            deltaY = 0;
+        } else if (i >= m && (j >= 0 && j < m)) {
+            neighbourCell = matrix[i - m][j];
+            deltaX = l;
+            deltaY = 0;
+        } else if (j < 0 && (i >= 0 && i < m)) {
+            neighbourCell = matrix[i][j + m];
+            deltaX = 0;
+            deltaY = l * -1;
+        } else if (j >= m && (i >= 0 && i < m)) {
+            neighbourCell = matrix[i][j - m];
+            deltaX = 0;
+            deltaY = l;
+        }
+
+        for (Particle p : neighbourCell) {
+            neighbours.add(new Particle(p.getId(), p.getRadius(), p.getPosition().x + deltaX, p.getPosition().y + deltaY));
+        }
+
+    }
+
     public void calculateDistances() {
-        int cant = 0;
         long start = System.currentTimeMillis();
-        for(Particle particle : particles) {
-            for(Particle neighbour : getNeighbours(particle)) {
+        int cant = 0;
+        for (Particle particle : particles) {
+            for (Particle neighbour : getNeighbours(particle)) {
+                if (alreadyCalculated(particle, neighbour)) continue;
                 double distance = Particle.getDistance(particle, neighbour);
-//                if(distance < rc)
-//                    System.out.println(cant++ + " " + distance);
+                if (distance < rc) {
+                    cant++;
+                    if (neighbours.containsKey(particle.getId())) {
+                        neighbours.get(particle.getId()).add(neighbour.getId());
+                    } else {
+                        Set<Integer> particleSet = new HashSet<>();
+                        particleSet.add(neighbour.getId());
+                        neighbours.put(particle.getId(), particleSet);
+
+                        Set<Integer> particleSetNeighbour = new HashSet<>();
+                        particleSetNeighbour.add(particle.getId());
+                        neighbours.put(neighbour.getId(), particleSetNeighbour);
+                    }
+                }
+
             }
         }
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
+        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start) + " M: " + m + " N: " + particles.size() + " cant: " + cant);
+        for (Integer p : neighbours.keySet()) {
+            System.out.print(p + " ");
+            for (Integer n : neighbours.get(p)) {
+                System.out.print(n + " ");
+            }
+            System.out.print("\n");
+        }
 
+    }
+
+    private boolean alreadyCalculated(Particle particle, Particle neighbour) {
+        return neighbours.containsKey(particle) && neighbours.get(particle).contains(neighbour);
     }
 
     public void calculateDistancesWithBruteForce() {
         int cant = 0;
         long start = System.currentTimeMillis();
-        for(Particle particle : particles) {
-            for(Particle neighbour : particles) {
-                if(particle == neighbour) continue;
+        for (Particle particle : particles) {
+            for (Particle neighbour : particles) {
+                if (particle == neighbour) continue;
                 double distance = Particle.getDistance(particle, neighbour);
-//                if(distance < rc)
-//                    System.out.println(cant++ + " " + distance);
+                if (distance < rc)
+                    cant++;
             }
         }
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
+        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start) + " cant " + cant);
     }
 
 
--- src/main.java
+++ src/main.java
@@ -1,22 +1,25 @@
 import cellindexmethod.CellIndexMethod;
 import models.Particle;
-import models.Point;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.DoubleAccumulator;
 
 /**
  * Created by amounier on 3/12/17.
  */
 public class main {
 
-    private static final int CANT_PARTICLES = 1000;
+    private static final int CANT_PARTICLES = 300;
     private static final int L = 20;
     private static final double RADIUS = 0.25;
     private static final double RC = 1;
-    private static final int M = 20;
+    /**
+     * La granularidad de la grilla (dada por M) tiene que ser de forma tal que a la hora de tomar una celda
+     * aledaña para calcular sus vecinos, esta celda contenga la menor cantidad de particulas cuya distancia a la
+     * particula en cuestion sea mayor que Rc.
+     */
+    private static int M = (int) Math.ceil(L / (RC + 2 * RADIUS));
 
 
     static List<Particle> particles = new ArrayList<>();
@@ -38,14 +41,19 @@ public class main {
             particles.add(particle);
         }
 
-        CellIndexMethod cellIndexMethod = new CellIndexMethod(L, RC, M, particles);
+
+//        for(int i = M; i <= 20 ; i++) {
+//            CellIndexMethod cellIndexMethod = new CellIndexMethod(L, RC, i, particles, false);
+//            cellIndexMethod.calculateDistances();
+//        }
+        CellIndexMethod cellIndexMethod = new CellIndexMethod(L, RC, M, particles, true);
         cellIndexMethod.calculateDistances();
         cellIndexMethod.calculateDistancesWithBruteForce();
     }
 
     public static boolean isValid(Particle p) {
-        for(Particle p2 : particles) {
-            if(Particle.getDistance(p, p2) < 0)
+        for (Particle p2 : particles) {
+            if (Particle.getDistance(p, p2) < 0)
                 return false;
         }
         return true;
